ceabcead6691a2f0b8d0c3315d151c8698f8946a
Cfnatic7 <78828448+Cfnatic7@users.noreply.github.com>
2022-11-12 23:24:30 +0100

    feat: moved forms in js to html

--- index.js
+++ index.js
@@ -1,55 +1,22 @@
+const loginForm = document.getElementById('login-form');
+const registerForm = document.getElementById('register-form');
+const welcomeText = document.getElementById('welcome-text');
+
+loginForm.style.display = 'none';
+registerForm.style.display = 'none';
+
+
+
+
 const chooseLoginOnCLick = () => {
-    let main = document.getElementById('main');
-    main.innerHTML = 
-    `<form class = 'login-form' action="login.php" method="POST"> 
-        <div> 
-            <span>Username</span>
-            <input type="text" name="username"
-            minlength="8" maxlength="20" required>
-        </div>
-        <div>
-            <span>Password</span>
-            <input type="password" name="password" 
-            minlength="8" maxlength="20" required>
-        </div>
-        <div>
-            <button type="submit" id="login">Login</button>
-        </div>
-    </form>`
+    welcomeText.style.display = 'none';
+    registerForm.style.display = 'none';
+    loginForm.style.display = 'flex';
 }
 
 const chooseRegisterOnClick = () => {
     let main = document.getElementById('main');
-    main.innerHTML = 
-    `<form class = 'register-form' method="POST"> 
-        <div> 
-            <span>Name</span>
-            <input type="text" name="username"
-            minlength="2" maxlength="20" required>
-        </div>
-        <div> 
-            <span>Surname</span>
-            <input type="text" name="username"
-            minlength="2" maxlength="20" required>
-        </div>
-        <div> 
-            <span>email</span>
-            <input type="email" name="username"
-            maxlength="40"
-            required>
-        </div>
-        <div> 
-            <span>Username</span>
-            <input type="text" name="username"
-            minlength="8" maxlength="20" required>
-        </div>
-        <div>
-            <span>Password</span>
-            <input type="password" name="password" 
-            minlength="8" maxlength="20" required>
-        </div>
-        <div>
-            <button type="submit" id="register">Register</button>
-        </div>
-    </form>`
+    welcomeText.style.display = 'none';
+    loginForm.style.display = 'none';
+    registerForm.style.display = 'flex';
 }
--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Caveat&display=swap" rel="stylesheet">
-    <script src="./index.js"></script>
+    <script src="./index.js" defer></script>
     <title>Book repository</title>
 </head>
 <body>
@@ -24,9 +24,56 @@
             <button type="button" onclick="chooseRegisterOnClick()" class="register-button">Register</button>
         </div>
     </header>
-    
+
     <main id="main">
         <span id="welcome-text">Home for all your books</span>
+        <form class = 'login-form' id = 'login-form' action="login.php" method="POST"> 
+            <div> 
+                <span>Username</span>
+                <input type="text" name="username"
+                minlength="8" maxlength="20" required>
+            </div>
+            <div>
+                <span>Password</span>
+                <input type="password" name="password" 
+                minlength="8" maxlength="20" required>
+            </div>
+            <div>
+                <button type="submit" id="login">Login</button>
+            </div>
+        </form>
+        <form class = 'register-form' id = 'register-form' method="POST"> 
+            <div> 
+                <span>Name</span>
+                <input type="text" name="username"
+                minlength="2" maxlength="20" required>
+            </div>
+            <div> 
+                <span>Surname</span>
+                <input type="text" name="username"
+                minlength="2" maxlength="20" required>
+            </div>
+            <div> 
+                <span>email</span>
+                <input type="email" name="username"
+                maxlength="40"
+                required>
+            </div>
+            <div> 
+                <span>Username</span>
+                <input type="text" name="username"
+                minlength="8" maxlength="20" required>
+            </div>
+            <div>
+                <span>Password</span>
+                <input type="password" name="password" 
+                minlength="8" maxlength="20" required>
+            </div>
+            <div>
+                <button type="submit" id="register">Register</button>
+            </div>
+        </form>
+
     </main>
     <footer>
         Copyright &copy; Book repository 2022. All rights reserved.
