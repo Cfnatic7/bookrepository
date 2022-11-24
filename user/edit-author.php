7a7fb5ab3fd305c3781568d80a0847263c7e2f8b
Cfnatic7 <78828448+Cfnatic7@users.noreply.github.com>
2022-11-24 12:47:41 +0100

    fix(edit-author): fix to editing author

--- user/edit-author.php
+++ user/edit-author.php
@@ -1,6 +1,6 @@
 <?php 
     session_start();
-    if (!isset($_SESSION['loggedIn']) || !isset($_seSSION['role']) || $_SESSION['role'] != 'admin') {
+    if (!isset($_SESSION['loggedIn']) || !isset($_SESSION['role']) || $_SESSION['role'] != 'admin') {
         header('Location: ../index.php');
         exit();
     }
